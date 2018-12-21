71c0fd9da37602f72686de25f9406cd110faf5a7
ha4gu <9391978+ha4gu@users.noreply.github.com>
2018-12-21 15:36:27 +0900

    add temporary action for deploying to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # temporary action for deploying to Heroku
+  def hello
+    render html: "hello world!"
+  end
+
 end
