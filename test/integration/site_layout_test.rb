c9a1f0cdd133db2ff5eb9be5526d576618766a00
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-11 12:01:51 +0900

    improve layout test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,13 +2,27 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links when not logged in" do
     get root_path
     assert_template 'static_pages/home'
+    # on header
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+    # on middle
+    assert_select "a[href=?]", signup_path
+    # on foother
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    # others
     assert_select "title", full_title
     get contact_path
     assert_select "title", full_title("Contact")
@@ -16,5 +30,25 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select "title", full_title("Sign up")
   end
+
+  test "layout links when logged in" do
+    log_in_as @user
+    get root_path
+    assert_template 'static_pages/home'
+    # on header
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    # on middle
+    assert_select "a[href=?]", signup_path
+    # on foother
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+
 
 end
