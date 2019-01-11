3eb311bf740026e9c6562ed54934ba08aa01aefb
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-11 09:35:44 +0900

    implement users#update

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   # password
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡された文字列のパスワードハッシュ値を返す
   def self.digest(string)
