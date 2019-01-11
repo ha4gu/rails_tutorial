bfc5d5b4fb4d5eda421ba6a7309fd57ba127281a
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-11 10:10:51 +0900

    restrict user edit only for logged in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -40,5 +42,13 @@ class UsersController < ApplicationController
       :password_confirmation)
   end
 
+  # before actions
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,30 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
+
+  test "should redirect from edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect from update when not logged in" do
+    get edit_user_path(@user)
+
+    patch user_path(@user), params: { user: { name: @user.name,
+      email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
 
 end
