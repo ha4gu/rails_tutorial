bd5ddae9807e728bdc1cdbe2db2c66cdba25a4ff
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-13 11:32:44 +0900

    implement password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,71 @@
+class PasswordResetsController < ApplicationController
+
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      # パスワード入力欄が空のケース
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      # 適切にパスワードの更新ができた場合
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      # パスワード更新に失敗した場合(文字数が足りない、など)
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # 正しいユーザかどうか確認する
+  #  ※ユーザアカウントが存在しており、アクティベーション済みであり、
+  #    かつreset_tokenが一致していれば受け入れるが、それ以外は弾く。
+  def valid_user
+    # @userはget_userで定義済み。よってget_userとセットで使用すること。
+    unless (@user && @user.activated? &&
+      @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
+  # トークンの有効期限が切れていないか確認する
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   # トークンを保持するための remember_token 属性、activation_token属性を定義
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   # force email strings to be saved as lower-case
   before_save :downcase_email
@@ -56,6 +56,23 @@ class User < ApplicationRecord
   # アクティベーションのためのメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # パスワード再設定の期限(2時間)が切れている場合はtrue
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
