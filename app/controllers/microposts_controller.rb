8e761d33bc1be15dba44fc6c7e3cd9286bffe629
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-14 17:41:58 +0900

    implement deletion of micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,7 @@ class MicropostsController < ApplicationController
 
   # logged_in_userはapplication_controller.rbに定義あり
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -15,12 +16,20 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted."
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 
 end
