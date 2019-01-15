25e39a17a256b881e5937031d0b4d3b31f71e2c4
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-15 10:28:29 +0900

    make it possible to post with image

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,8 +5,20 @@ class Micropost < ApplicationRecord
   # ordering; 作成日時の降順
   default_scope lambda { order(created_at: :desc) }
 
+  # 画像アップローダ
+  mount_uploader :picture, PictureUploader
+
   # validation
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 
 end
