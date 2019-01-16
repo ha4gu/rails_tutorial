4e7c9a98e56fd757fe6b37c01a093041a414775c
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-16 10:34:47 +0900

    add passive_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
     foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+    foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # トークンを保持するための remember_token 属性、activation_token属性を定義
   attr_accessor :remember_token, :activation_token, :reset_token
@@ -84,6 +88,18 @@ class User < ApplicationRecord
   # ユーザのフィードデータ ※現時点では不完全
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed: other_user).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
